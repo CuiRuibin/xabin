d2dfe4f40330b49b3d8bc3f2ba622e20dcecc0b4
cuiruibin <cuiruibin@localhost.localdomain>
2016-11-13 23:24:59 +0800

    add testcases & style sheet

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.utils import timezone
+import datetime
 # Create your models here.
 
 class Question(models.Model):
@@ -11,6 +12,10 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
