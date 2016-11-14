811e2c42be4f58b77ab438e79fe517faf7e10fd7
cuiruibin <cuiruibin@localhost.localdomain>
2016-11-15 00:38:13 +0800

    add admin page fixup, try to fix the default template file

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
@@ -41,7 +41,7 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form
-        return render(requestion, 'polls/detail.html', {
+        return render(request, 'polls/detail.html',{
             'question':question,
             'error_message': "You didn't select a choice.",
             })
